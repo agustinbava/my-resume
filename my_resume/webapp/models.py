2acb6224457c9ff3056c8ee604974fe8a51390a5
agustinbava <agustinbava@gmail.com>
2023-03-23 16:05:48 -0300

    In this commit I: - Hardcoded all the information needed in the index.html static page - Created the model to pass that static information (at least from the About section) - Created docker-compose file to run mysql db - Removed unnecessary data from index.html

--- my_resume/webapp/models.py
+++ my_resume/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+    header = models.TextField(null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    title_description = models.TextField(null=False, blank=False)
+    phone = models.CharField(max_length=50, null=False, blank=False)
+    city = models.CharField(max_length=50, null=False, blank=False)
+    age = models.IntegerField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    
+    def __str__(self, *args, **kwargs):
+        return f"{self.id}, {self.title}"
+    
+    def __unicode__(self, *args, **kwargs):
+        return f"{self.id}, {self.title}"
