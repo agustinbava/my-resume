c0321cf79115dca4ade78e462fe324027bf7d331
agustinbava <agustinbava@gmail.com>
2023-03-24 18:14:55 -0300

    Added form model, edited About model and changed index.html

--- my_resume/webapp/models.py
+++ my_resume/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 class About(models.Model):
     header = models.TextField(null=False, blank=False)
@@ -8,9 +9,21 @@ class About(models.Model):
     city = models.CharField(max_length=50, null=False, blank=False)
     age = models.IntegerField(null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
+    degree = models.CharField(max_length=50, default='')
+    birthday = models.DateField(default=now)
+    text_description = models.TextField(max_length=25000, default='')
     
     def __str__(self, *args, **kwargs):
         return f"{self.id}, {self.title}"
     
     def __unicode__(self, *args, **kwargs):
         return f"{self.id}, {self.title}"
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    subject = models.CharField(max_length=500, blank=True, null=False)
+    message = models.TimeField(max_length=15000, blank=False, null=False)
+    
+    def __str__(self):
+        return self.email + ' ' + self.subject 
--- my_resume/webapp/views.py
+++ my_resume/webapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ( TemplateView )
+from .forms import ContactForm
 
 class HomePageTemplateView(TemplateView):
     template_name = 'index.html'
+    
+def contact(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message = f"Thank you, {form.name}, for submitting the email!"
+            context = {'message': message}
+    return render(request, 'index.html', context)
