fb582c4ef5ebc0f2fae2803151e775591ba24ef9
agustinbava <agustinbava@gmail.com>
2023-03-25 18:21:34 -0300

    Fixed some bugs related to Contact functionality. Still in WIP

--- my_resume/webapp/views.py
+++ my_resume/webapp/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.views.generic import ( TemplateView )
 from .forms import ContactForm
 
@@ -11,15 +8,9 @@ class HomePageTemplateView(TemplateView):
 def contact_submit(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        print(form)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
             form.save()
-            messages.success(request, 'Your message was sent successfully!')
             return render(request, 'success.html', {'form': form})
-    else:
-        form = ContactForm()
+        else:
+            form = ContactForm()
     return render(request, 'index.html', {'form': form})
