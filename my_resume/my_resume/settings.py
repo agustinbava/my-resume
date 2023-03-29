e807c01352c8ece645843f8e0fbc2e1b06ef3e7c
agustinbava <agustinbava@gmail.com>
2023-03-29 15:57:23 -0300

    DEBUG False

--- my_resume/my_resume/settings.py
+++ my_resume/my_resume/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_fsan4l=ox5$6usvf23@30x&pm)5!c-281%q5+^yl%8=s&1)7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", ".agustinbava.click"]
 
